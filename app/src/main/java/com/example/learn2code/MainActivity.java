9aae82fbdc2770cf4c073c56ec4b759a5c708eea
Victoria <2004466@uad.ac.uk>
2022-11-17 21:30:01 +0000

    Proof of concept

---
+++ app/src/main/java/com/example/learn2code/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.learn2code;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button lesson_button = findViewById(R.id.lesson_button);
+
+        lesson_button.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Lesson.class)));
+    }
+}
