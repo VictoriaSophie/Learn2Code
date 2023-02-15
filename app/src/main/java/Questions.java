d9cafa78787884a63192fb0646175b3034b7738e
Victoria <2004466@uad.ac.uk>
2023-02-15 21:30:32 +0000

    Added a question to questions class

--- app/src/main/java/Questions.java
+++ app/src/main/java/Questions.java
@@ -1,2 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Questions {
+    public static Map<String, Map<String,Boolean>> getQuestions(){
+        HashMap<String,Map<String,Boolean>> questions = new HashMap<>();
+
+        HashMap<String,Boolean> answerOne = new HashMap<>();
+        answerOne.put("<", true);
+        answerOne.put("!", false);
+        answerOne.put(":", false);
+        answerOne.put(";", false);
+        questions.put("What character is commonly used to identify a tag in HTML?", answerOne);
+
+        return questions;
+
+    }
 }
