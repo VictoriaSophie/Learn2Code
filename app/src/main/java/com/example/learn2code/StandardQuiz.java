0a5f31674e49dada0afec77192dae61f1736f476
Victoria <2004466@uad.ac.uk>
2023-02-15 22:08:29 +0000

    added quiz logic to standard quiz activity

--- app/src/main/java/com/example/learn2code/StandardQuiz.java
+++ app/src/main/java/com/example/learn2code/StandardQuiz.java
@@ -7,23 +7,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class StandardQuiz extends AppCompatActivity {
     String txt;
     private Button butNext;
-    private CardView radioButtonA, radioButtonB, radioButtonC, radioButtonD;
+    private Button radioButtonA, radioButtonB, radioButtonC, radioButtonD;
 
+    private TextView question, newQuestionNumber, text1, text2, text3, text4;
+    private int questionNumber = 0; // tracks what question we are on
+    private int correctAnswer = 0;
+    private List<String> questions;
+    private Map<String, Map<String, Boolean>> questionsAnswerMap;
+    private ArrayList<String> getQuestionsAnswerMap;
+
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standard_quiz);
         Intent intent = getIntent();
 
+        questionsAnswerMap = Questions.getQuestions();
         initView();
-        variantClickA();
-        variantClickB();
-        variantClickC();
-        variantClickD();
+        optionClickA();
+        optionClickB();
+        optionClickC();
+        optionClickD();
 
         findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
 
@@ -31,12 +47,18 @@ public class StandardQuiz extends AppCompatActivity {
             public void onClick(View view) {
 
                 if (!txt.isEmpty()) {
-                    // get answer
-                    // if answer right, add one
+                    boolean answer = questionsAnswerMap.get(questions.get(questionNumber)).get(txt);
+                    if (answer) {
+                        correctAnswer++;
+                    }
+                    questionNumber++;
+                    optionsClear();
                 }
                 if (butNext.getText().equals("Next")) {
                     displayNextQuestions();
-                } // else results
+                } else {
+                    // result page Intent intentResults = new Intent(StandardQuiz.this, )
+                }
             }
         });
 
@@ -44,27 +66,61 @@ public class StandardQuiz extends AppCompatActivity {
 
     }
 
+    private void optionsClear() {
+        txt = "";
+    }
+
     private void displayData() {
     }
 
     private void displayNextQuestions() {
+        setAnswerstoOptions();
+        question.setText(questions.get(questionNumber));
+        newQuestionNumber.setText("" + (questionNumber + 1));
+        progressBar.setProgress(questionNumber * 10);
+        progressBar.setMax(100);
+
+        // when finished qs
     }
 
-    private void variantClickD() {
-        radioButtonD.setOnClickListener( v -> {
+    private void setAnswerstoOptions() {
+        ArrayList<String> questionKey = new ArrayList(questionsAnswerMap.get(questions.get(questionNumber)).keySet());
 
+        radioButtonA.setText(questionKey.get(0));
+        radioButtonB.setText(questionKey.get(1));
+        radioButtonC.setText(questionKey.get(2));
+        radioButtonD.setText(questionKey.get(3));
+    }
+
+    private void optionClickD() {
+        radioButtonD.setOnClickListener( v -> {
+            txt = text4.getText().toString();
         });
     }
 
-    private void variantClickC() {
+    private void optionClickC() {
+        radioButtonC.setOnClickListener( v -> {
+            txt = text3.getText().toString();
+        });
     }
 
-    private void variantClickB() {
+    private void optionClickB() {
+        radioButtonB.setOnClickListener( v -> {
+            txt = text2.getText().toString();
+        });
     }
 
-    private void variantClickA() {
+    private void optionClickA() {
+        radioButtonA.setOnClickListener( view -> {
+            txt = text1.getText().toString();
+        });
     }
 
     private void initView() {
+        View radioButtonA = findViewById(R.id.radioButtonOptionA);
+        View radioButtonB = findViewById(R.id.radioButtonOptionA);
+        View radioButtonC = findViewById(R.id.radioButtonOptionA);
+        View radioButtonD = findViewById(R.id.radioButtonOptionA);
+
     }
 }
