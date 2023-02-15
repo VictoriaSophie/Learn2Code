cd3069372bfca06007d9b1657230abe07e66db32
Victoria <2004466@uad.ac.uk>
2023-02-15 22:45:43 +0000

    Added button to standard quiz on main

--- app/src/main/java/com/example/learn2code/MainActivity.java
+++ app/src/main/java/com/example/learn2code/MainActivity.java
@@ -15,9 +15,11 @@ public class MainActivity extends AppCompatActivity {
 
         Button lesson_button = findViewById(R.id.lesson_button);
         Button crud_button = findViewById(R.id.crud_button);
+        Button quiz_button = findViewById(R.id.quizButton);
 
         lesson_button.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Lesson.class)));
         crud_button.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, CRUD.class)));
+        quiz_button.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, StandardQuiz.class)));
 
     }
 }
