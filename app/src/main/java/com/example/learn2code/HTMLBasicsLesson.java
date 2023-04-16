134bb7c30a1872055b7e64c33b222f2e8c83eaf1
Victoria <2004466@uad.ac.uk>
2023-04-16 23:23:14 +0100

    Renamed LessonOneHTML to HTMLBasicsLesson

--- app/src/main/java/com/example/learn2code/HTMLBasicsLesson.java
+++ app/src/main/java/com/example/learn2code/HTMLBasicsLesson.java
@@ -6,13 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class LessonOneHTML extends AppCompatActivity {
+public class HTMLBasicsLesson extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_one_html);
         Button quiz_button = findViewById(R.id.testButton);
-        quiz_button.setOnClickListener(view -> startActivity(new Intent(LessonOneHTML.this, StandardQuiz.class)));
+        quiz_button.setOnClickListener(view -> startActivity(new Intent(HTMLBasicsLesson.this, StandardQuiz.class)));
     }
 }
