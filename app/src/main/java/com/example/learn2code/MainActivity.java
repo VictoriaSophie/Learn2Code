f1c6b2afbe925780e3197c06712812057eb488c2
Victoria <2004466@uad.ac.uk>
2023-04-25 14:04:37 +0100

    Created SetAppBar method and functionality to display toast message for every button

--- app/src/main/java/com/example/learn2code/MainActivity.java
+++ app/src/main/java/com/example/learn2code/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
@@ -16,38 +15,45 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
-//    BottomAppBar bottomAppBar;
-//    FloatingActionButton home_button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        bottomAppBar=findViewById(R.id.bottomAppBar2);
-//        home_button=findViewById(R.id.home);
-//        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-//            @Override
-//            public boolean onMenuItemClick(MenuItem item) {
-//                if (item.getItemId()==R.id.settings)
-//                {
-//                    Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_LONG).show();
-//                }
-//                return false;
-//            }
-//        });
-//        home_button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//             Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_LONG).show();
-//            }
-//        });
+        setAppBar();
 
         Button html_button = findViewById(R.id.htmlBasics_button);
-        html_button.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, LessonOneHTML.class)));
-//        Toolbar toolbar;
-//        toolbar = findViewById(R.id.bottomAppBar2);
-//        setSupportActionBar(toolbar);
 
+        html_button.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, LessonOneHTML.class)));
+
+
+    }
+
+    private void setAppBar() {
+        BottomAppBar bottomAppBar;
+        bottomAppBar = findViewById(R.id.bottomAppBar2);
+        FloatingActionButton home_button = findViewById(R.id.home);
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.settings) {
+                    Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+
+        bottomAppBar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "back", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        home_button.setOnClickListener(view -> Toast.makeText(MainActivity.this, "Home button", Toast.LENGTH_LONG).show());
+
+    }
+
+    private void setOnMenuItemClickListener() {
     }
 }
