55a5acee66ca10386f3e5d8d85e5a79846106394
Victoria <2004466@uad.ac.uk>
2023-04-25 14:30:48 +0100

    Added toolbar functionality to Standard Quiz

--- app/src/main/java/com/example/learn2code/StandardQuiz.java
+++ app/src/main/java/com/example/learn2code/StandardQuiz.java
@@ -1,12 +1,14 @@
 package com.example.learn2code;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -14,6 +16,9 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +41,7 @@ public class StandardQuiz extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standard_quiz);
+        setAppBar();
 
         xphandler = new XPDatabase(StandardQuiz.this);
         questionAmountTextView =findViewById(R.id.totalQuestions);
@@ -126,6 +132,31 @@ public class StandardQuiz extends AppCompatActivity implements View.OnClickListe
             clickedButton.setBackgroundColor(Color.CYAN);
 
         }
+
+    }
+    private void setAppBar() {
+        BottomAppBar bottomAppBar;
+        bottomAppBar = findViewById(R.id.bottomAppBar2);
+        FloatingActionButton home_button = findViewById(R.id.home);
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.settings) {
+                    Toast.makeText(StandardQuiz.this, "Settings", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+
+        bottomAppBar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(StandardQuiz.this, "back", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        home_button.setOnClickListener(view -> Toast.makeText(StandardQuiz.this, "Home button", Toast.LENGTH_LONG).show());
 
     }
 }
