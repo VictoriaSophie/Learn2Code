421f6747e67e26a642cc555070cfa6b989a48497
Victoria <2004466@uad.ac.uk>
2023-04-27 18:45:22 +0100

    Added button to programming basics

--- app/src/main/java/com/example/learn2code/ProgrammingBasicsLesson.java
+++ app/src/main/java/com/example/learn2code/ProgrammingBasicsLesson.java
@@ -2,7 +2,9 @@ package com.example.learn2code;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ProgrammingBasicsLesson extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class ProgrammingBasicsLesson extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_programming_basics_lesson);
+        Button quiz_button = findViewById(R.id.startButton);
+        quiz_button.setOnClickListener(view -> buttonPressed());
+    }
+
+    private void buttonPressed() {
+        Intent i = new Intent(getBaseContext(), StandardQuiz.class);
+        int quizNo = 1;
+        i.putExtra("quizNumber", quizNo);
+        startActivity(i);
     }
 }
