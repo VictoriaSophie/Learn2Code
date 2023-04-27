207dc06799ab07ac7627eea4aa392007986a7529
Victoria <2004466@uad.ac.uk>
2023-04-27 18:45:03 +0100

    Added questions for programming basics

--- app/src/main/java/com/example/learn2code/Questions.java
+++ app/src/main/java/com/example/learn2code/Questions.java
@@ -9,6 +9,11 @@ import java.util.Random;
 
 public class Questions {
     public static String question[] = {
+            // programming basics
+            "What does a programming language do?",
+            "What does a markup language do?",
+            "Which of the options are NOT a programming language?",
+            // html basics
             "What character is commonly used to identify a tag in HTML?",
             "What is HTML most commonly used for?",
             "What is the opening tag for a paragraph element?",
@@ -17,6 +22,9 @@ public class Questions {
     };
 
     public static String options[][] = {
+            {"Present information for a browser to interpret", "Give instructions to a computer to produce an output", "A set of information that allows people to choose which TV show to watch", "idk"},
+            {"Present information for a browser to interpret", "Give instructions to a computer to produce an output", "A set of information that allows people to choose which TV show to watch", "idk"},
+            {"Java", "JavaScript", "HTML", "Python"},
             {"<", "!", "/", ";"},
             {"Creating websites", "Machine learning", "Developing applications", "Databases"},
             {"<h1>", "<paragraph>", "p", "<p>"},
@@ -25,6 +33,9 @@ public class Questions {
     };
 
     public static String answers[] = {
+            "Give instructions to a computer to produce an output",
+            "Present information for a browser to interpret",
+            "HTML",
             "<",
             "Creating websites",
             "<p>",
