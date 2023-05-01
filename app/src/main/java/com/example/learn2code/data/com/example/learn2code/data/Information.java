06c3f77484ffabfa6f4683319bd6c86c2055165c
Victoria <2004466@uad.ac.uk>
2023-05-01 21:24:48 +0100

    Added programming basics to information page

--- app/src/main/java/com/example/learn2code/data/com/example/learn2code/data/Information.java
+++ app/src/main/java/com/example/learn2code/data/com/example/learn2code/data/Information.java
@@ -7,6 +7,8 @@ import com.example.learn2code.R;
 
 public class Information {
     public static String title[][] = {
+            // programming basics
+            {"How are websites created?", "What is Learn2Code?", "How do I use this application?"},
             //css basics
             {"What is CSS?",
             "Oops!", "third thing"},
@@ -14,27 +16,29 @@ public class Information {
     };
 
     public static String cardOneInfo[] = {
-            "So far, we have learned how to create basic elements on a website but we want our application to look pretty. To do this, we can use CSS (Cascading Style Sheets) which tells the computer how we want to design our website.",
+            "Websites and other applications are created by developers who write code to create any features you see on the screen. There are a variety of programming languages that developers uses which all have their uses and drawbacks.", // programming basics
+            "So far, we have learned how to create basic elements on a website but we want our application to look pretty. To do this, we can use CSS (Cascading Style Sheets) which tells the computer how we want to design our website.", // CSS Basics
             "I forgot index starts at 0"
 
     };
 
     public static String cardTwoInfo[] = {
-            "This is in the second box"
+            "This application will teach you how to create your very own website. It will give you all the tools and knowledge needed to create any website you want."
 
     };
 
     public static String cardThreeInfo[] = {
-            "The third box is my favourite!"
+            "Learn2Code uses a variety of techniques to allow you to read and understand the information which is being given to you. An example of one of these techniques is quizzes, please click the button below to begin your first quiz!"
     };
 
-    public static int[] image = {
-            R.drawable.css_logo
+    public static int[][] image = {
+            {R.drawable.building_website, R.drawable.placeholder, R.drawable.placeholder},
+            {R.drawable.css_logo}
 
 
     };
 
-    public static Button buttons[] = {
+    public static Button buttons[][] = {
 
     };
 }
