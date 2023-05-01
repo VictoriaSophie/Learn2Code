b452d99006b9561d23e1b168eda0417528d1deb0
Victoria <2004466@uad.ac.uk>
2023-05-01 20:27:29 +0100

    Created Standard Info Page and class for input

--- app/src/main/java/com/example/learn2code/StandardInfoPage.java
+++ app/src/main/java/com/example/learn2code/StandardInfoPage.java
@@ -1,19 +1,65 @@
 package com.example.learn2code;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.learn2code.data.Information;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class StandardInfoPage extends AppCompatActivity {
+
+    Button cardOneButton, cardTwoButton, cardThreeButton;
+    TextView cardOneTitle, cardTwoTitle, cardThreeTitle, cardOneText, cardTwoText, cardThreeText;
+    ImageView cardOneImage, cardTwoImage, cardThreeImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standard_info_page);
         setAppBar();
+
+        cardOneTitle=findViewById(R.id.cardOneTitle);
+        cardTwoTitle=findViewById(R.id.cardTwoTitle);
+        cardThreeTitle=findViewById(R.id.cardThreeTitle);
+        cardOneText=findViewById(R.id.cardOneText);
+        cardTwoText=findViewById(R.id.cardTwoText);
+        cardThreeText=findViewById(R.id.cardThreeText);
+        cardOneImage=findViewById(R.id.cardOneImage);
+
+        Intent intent = getIntent();
+        int pageNumber = intent.getIntExtra("pageNumber", 0);
+//        switch (pageNumber) {
+//            case 0:
+//                cardOneTitle.setText("f");
+//                break;
+//            case 1:
+//                cardOneTitle.setText(Information.title[pageNumber][0]);
+//                break;
+//            case 2:
+//                cardOneTitle.setText("number two");
+//                break;
+//        }
+        loadInformation(pageNumber);
+    }
+
+    private void loadInformation(int pageNumber) {
+//        cardOneButton.setText((CharSequence) Information.buttons[pageNumber]);
+        cardOneTitle.setText(Information.title[pageNumber][0]);
+        cardTwoTitle.setText(Information.title[pageNumber][1]);
+        cardThreeTitle.setText(Information.title[pageNumber][2]);
+        cardOneText.setText(Information.cardOneInfo[pageNumber]);
+        cardTwoText.setText(Information.cardTwoInfo[pageNumber]);
+        cardThreeText.setText(Information.cardThreeInfo[pageNumber]);
+        cardOneImage.setImageDrawable(ContextCompat.getDrawable(this, Information.image[pageNumber]));
+        //cardOneImage.setImageIcon(Information.image[pageNumber]);
+
     }
 
     private void setAppBar() {
---
+++ app/src/main/java/com/example/learn2code/data/com/example/learn2code/data/Information.java
@@ -0,0 +1,40 @@
+package com.example.learn2code.data;
+
+import android.media.Image;
+import android.widget.Button;
+
+import com.example.learn2code.R;
+
+public class Information {
+    public static String title[][] = {
+            //css basics
+            {"What is CSS?",
+            "Oops!", "third thing"},
+
+    };
+
+    public static String cardOneInfo[] = {
+            "So far, we have learned how to create basic elements on a website but we want our application to look pretty. To do this, we can use CSS (Cascading Style Sheets) which tells the computer how we want to design our website.",
+            "I forgot index starts at 0"
+
+    };
+
+    public static String cardTwoInfo[] = {
+            "This is in the second box"
+
+    };
+
+    public static String cardThreeInfo[] = {
+            "The third box is my favourite!"
+    };
+
+    public static int[] image = {
+            R.drawable.css_logo
+
+
+    };
+
+    public static Button buttons[] = {
+
+    };
+}
