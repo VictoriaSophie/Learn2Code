ac49043c48d7c6993ef3bc470a1d95ebf38c3c8a
Victoria <2004466@uad.ac.uk>
2023-05-06 14:50:54 +0100

    Add button to log in page

--- app/src/main/java/com/example/learn2code/Settings.java
+++ app/src/main/java/com/example/learn2code/Settings.java
@@ -2,7 +2,10 @@ package com.example.learn2code;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button logoutButton = findViewById(R.id.logoutButton);
+
+        logoutButton.setOnClickListener(view -> startActivity(new Intent(Settings.this, LogIn.class)));
     }
 }
