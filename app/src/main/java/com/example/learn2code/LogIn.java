c7042916186f7708e31f02549ef187c504b04b65
Victoria <2004466@uad.ac.uk>
2023-05-06 14:50:19 +0100

    Created log in activity

--- app/src/main/java/com/example/learn2code/LogIn.java
+++ app/src/main/java/com/example/learn2code/LogIn.java
@@ -2,7 +2,12 @@ package com.example.learn2code;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LogIn extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        TextView email = findViewById(R.id.email);
+        TextView password = findViewById(R.id.password);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        Button registerButton = findViewById(R.id.registerButton);
+
+        loginButton.setOnClickListener(view -> {
+            if(email.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                Toast.makeText(LogIn.this, "Correct email and password", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(LogIn.this, "Wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        registerButton.setOnClickListener(view -> startActivity(new Intent(LogIn.this, Register.class)));
     }
 }
