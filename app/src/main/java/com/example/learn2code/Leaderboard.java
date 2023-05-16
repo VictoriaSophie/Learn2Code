0a06cbcf5ded0967a2d17247bd6d8ab5eff515a3
Victoria <2004466@uad.ac.uk>
2023-05-16 02:59:11 +0100

    Began coding leaderboard functions

--- app/src/main/java/com/example/learn2code/Leaderboard.java
+++ app/src/main/java/com/example/learn2code/Leaderboard.java
@@ -2,15 +2,69 @@ package com.example.learn2code;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.learn2code.data.XPDatabase;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.LeaderboardsClient;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class Leaderboard extends CommonMethods {
+
+    FirebaseAuth auth;
+    FirebaseUser user;
+    Long xp;
+    TextView currentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_leaderboard);
         getLayoutResource();
+        FirebaseApp.initializeApp(this);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        getXpData();
+        currentPosition = findViewById(R.id.currentText);
+        currentPosition.setText("xp: " + xp);
 
+
+
+    }
+
+    private void getXpData() {
+        DatabaseReference dataRef = FirebaseDatabase.getInstance().getReference("xp");
+        if (user != null) {
+            dataRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // Retrieve the "xp" value from the dataSnapshot
+                    xp = dataSnapshot.getValue(Long.class);
+
+                    // Display the retrieved data
+//                    Toast.makeText(StandardQuiz.this, "User: " + userName + ", XP: " + userXP, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // Handle the error, if any
+//                    Toast.makeText(StandardQuiz.this, "Failed to retrieve data: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        } else {
+            xp = (long) XPDatabase.getXP("xp");
+        }
 
     }
 
